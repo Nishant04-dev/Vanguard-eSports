9b6283d6b061cacdb7fffb769a360940c1c3a30a
Nishant Chauhan <info.nishantchauhan@gmail.com>
2025-08-26 06:03:34 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -95,14 +95,14 @@ function App() {
       date: "August, 2025",
       prize: "₹21,000",
       status: "Registration Close",
-      image: "https://pin.it/6FQCCLZAq"
+      image: "https://pin.it/6FQCCLZAq?auto=compress&cs=tinysrgb&w=400"
     },
     {
       title: "Summer Showdown",
       date: "March, 2025",
       prize: "₹11,000",
       status: "Completed",
-      image: "https://images.pexels.com/photo/33614154/"
+      image: "https://images.pexels.com/photo/33614154/?auto=compress&cs=tinysrgb&w=400"
     }
   ];
 
